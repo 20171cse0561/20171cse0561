326da742f0451e6926a39554b92a3c3f7230dfc1
20171cse0561 <60248962+20171cse0561@users.noreply.github.com>
2020-01-24 10:54:27 +0530

    Add files via upload

---
+++ test1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+char choice;
+printf("enter the choice\n");
+scanf("%c",&choice);
+switch(choice)
+{
+case'c':printf("swatch barath, cleaning conopy\n");
+		break;
+case'r':printf("run 10 rounda the colege\n");
+		break;
+case'a':printf("write assignment of 100 question\n");
+		break;
+case's':printf("one week suspension from class\n");
+		break;
+default:printf("sit in front\n");
+}
+}
+
+
